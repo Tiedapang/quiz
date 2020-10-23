e3e8c705c2e07b850bf5ef5abf740839abacd489
tiedapang <2750428832@qq.com>
2020-10-23 12:22:39 +0800

    shoppingCart added

--- front-end/src/compontens/ShowPage.js
+++ front-end/src/compontens/ShowPage.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import bg from '../img/cole.jpg'
+import '../css/showPage.css'
+import cart from '../img/shopping_cart-24px.svg'
+import deletePic from '../img/delete-24px.svg'
 
 class ShowPage extends Component{
     
@@ -6,6 +10,7 @@ class ShowPage extends Component{
             super();
             this.state = {
                 product:[],
+                orders:[],
             }
           }
          
@@ -15,21 +20,44 @@ class ShowPage extends Component{
                 product:pro
             });
           }
-          render(){
+
+    addProdToOrder = (item) => {
+        console.log(item);
+    }
+          render() {
             const elements=[];
             this.state.product.forEach((item)=>{
               elements.push(
-                <div>
-                  <img></img>
-                  <p>{item.name}</p>
-                  <p>单位：{item.price}/{item.unit}</p>
-                    <button>+</button>
+                <div key={item.id} className={'pro-items'}>
+                  <img src={bg}></img>
+                  <p className={'pro-items-name'}>{item.name}</p>
+                  <p className={'pro-items-unit'}>单位：{item.price}/{item.unit}</p>
+                    <button className={'pro-items-add-button'} onClick={() => this.addProdToOrder(item)}>+</button>
                 </div>
               )
             });
               return(
-                <div>
-                   {elements}
+                <div className={'showPage'}>
+                    <div className={'elements'}>
+                        {elements}
+                    </div>
+                    <div className={'table-item'}>
+                        <table>
+                            <th>
+                                <td>商品</td>
+                                <td>数量</td>
+                                <td></td>
+                            </th>
+                            <tr>
+                                <td>可乐</td>
+                                <td>2</td>
+                                <td><img src={deletePic}/></td>
+                            </tr>
+                        </table>
+                    </div>
+                   <div className={'cart'}>
+                       <img src={cart}></img>
+                   </div>
                   
               </div>
  
